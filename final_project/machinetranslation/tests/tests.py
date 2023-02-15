661d4e592e14e432a78cdb2103486c7c412a4dcb
GerardTorres1 <a20194793@pucp.edu.pe>
2023-02-14 19:29:56 -0500

    Final changes

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -14,6 +14,11 @@ class TestEnglishToFrench(unittest.TestCase):
             english_to_french(None), ""
         )
 
+    def test3(self):
+        self.assertNotEqual(
+            english_to_french("Hello"), "Hello"
+        )
+
 
 class TestFrenchToEnglish(unittest.TestCase):
     def test1(self):
@@ -24,6 +29,11 @@ class TestFrenchToEnglish(unittest.TestCase):
     def test2(self):
         self.assertEqual(
             french_to_english(None), ""
+        )
+
+    def test3(self):
+        self.assertNotEqual(
+            french_to_english("Bonjour"), "Bonjour"
         )
 
 
